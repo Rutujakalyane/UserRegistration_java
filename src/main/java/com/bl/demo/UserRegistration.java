be10efd37e07a4e8fc7667a569d0227db0d2e17b
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-19 20:26:52 +0530

    REFACTORED : Removed extra characters

--- src/main/java/com/bl/demo/UserRegistration.java
+++ src/main/java/com/bl/demo/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration
 {
     String namePattern = "^[A-Z][a-z]{2,}$";
-    public String emailIdPattern = p="^[a-zA-Z0-9]+([.+,-,_]?[a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
+    public String emailIdPattern = "^[a-zA-Z0-9]+([.+,-,_]?[a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
 
     public boolean checkFirstName(String firstName)
     {
