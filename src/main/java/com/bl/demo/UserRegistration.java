3b79e9e2a44968083e9720f9cc71b9d6dd9d3c38
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-19 19:25:35 +0530

    ADDED : Feature to Validate last name

--- src/main/java/com/bl/demo/UserRegistration.java
+++ src/main/java/com/bl/demo/UserRegistration.java
@@ -1,12 +1,23 @@
 package com.bl.demo;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class UserRegistration {
-    String firstNamePattern = "^[A-Z][a-z]{2,}$";
+public class UserRegistration
+{
+    String namePattern = "^[A-Z][a-z]{2,}$";
+    public boolean checkFirstName(String firstName)
+    {
+        boolean result = firstName.matches(namePattern);
+        return result;
 
-    public boolean checkFirstName(String firstName) {
-        boolean result = firstName.matches(firstNamePattern);
+    }
+    public boolean checkLastName(String lastName)
+    {
+        boolean result = lastName.matches(namePattern);
         return result;
     }
 }
 
-}
+
+
+
--- src/test/java/com/bl/demo/UserRegistrationTest.java
+++ src/test/java/com/bl/demo/UserRegistrationTest.java
@@ -3,10 +3,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
-
-    public void givenValidFirstName_ShouldReturnTrue() {
+    @Test
+    public void givenValidFirstName_ShouldReturnTrue()
+    {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.checkFirstName("Rutuja");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenValidLastName_ShouldReturnTrue()
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.checkLastName("Kalyane");
         Assert.assertTrue(result);
     }
 }
